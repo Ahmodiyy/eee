252f7ae2a28fc94b915cfcb3e11965a8f9e8b397
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-04-21 07:34:23 +0100

    demo

---
+++ src/main/java/com/example/eee/Erest.java
@@ -0,0 +1,12 @@
+package com.example.eee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Erest {
+    @GetMapping("/")
+    public String getName(){
+        return "eee aplication";
+    }
+}
---
+++ src/main/java/com/example/eee/Person.java
@@ -0,0 +1,11 @@
+package com.example.eee;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    String getName(){
+        return "spring";
+    }
+}
